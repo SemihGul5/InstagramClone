5130886e58501c6600ac5337ae0fabe1d01ce8a1
Semih Gül <semih.gul099@gmail.com>
2023-10-03 15:06:58 +0300

    kayıt ol yapıldı

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,28 +1,71 @@
 package com.example.instagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.instagramclone.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
-    private void loginClicked(View view){
+    public void loginClicked(View view){
 
     }
-    private void signInClicked(View view){
+    public void signUpClicked(View view){
+        String eMail= binding.eMailText.getText().toString();
+        String password=binding.passwordText.getText().toString();
+
+        if (eMail.equals("")||password.equals("")){
+            Toast.makeText(MainActivity.this,"Tüm alanları doldurun",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            mAuth.createUserWithEmailAndPassword(eMail,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    //giriş başarılıysa
+                    Intent intent= new Intent(MainActivity.this,FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    //giriş başarısızsa
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
     }
+
 
 
 
