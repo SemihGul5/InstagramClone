28bbfa90e70bebc57e1eff121440e2055e4bb9e6
Semih Gül <semih.gul099@gmail.com>
2023-10-04 15:00:32 +0300

    izin işlemleri yapıldı gönderi paylaşma ve firebase de kaydetme işlemleri yapıldı

--- app/src/main/java/com/example/instagramclone/PostActivity.java
+++ app/src/main/java/com/example/instagramclone/PostActivity.java
@@ -1,14 +1,53 @@
 package com.example.instagramclone;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.instagramclone.databinding.ActivityPostBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class PostActivity extends AppCompatActivity {
+    //Bitmap selectedImage;
     private ActivityPostBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Uri imageData;
+    FirebaseAuth auth;
+    FirebaseStorage firebaseStorage;
+    FirebaseFirestore firebaseFirestore;
+    StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +55,146 @@ public class PostActivity extends AppCompatActivity {
         binding=ActivityPostBinding.inflate(getLayoutInflater());
         View view=binding.getRoot();
         setContentView(view);
+
+        registerLauncher();
+
+        auth=FirebaseAuth.getInstance();
+        firebaseStorage=FirebaseStorage.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        storageReference=firebaseStorage.getReference();// görseli depoda nereye kaydediceğimizi gösteren bir değişken
     }
 
-    private void postButtonClicked(View view){
+
+
+    public void postButtonClicked(View view){
+        if (imageData!=null){
+            //unique bir id oluşturur
+            UUID uuid= UUID.randomUUID();
+            String path="images/"+uuid+".jpg";
+            storageReference.child(path).putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                //görseli storage'a kaydettik, onSuccess de veritabanına kaydetme işlemleri, ilgili kullanıcının
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //direkt o kaydedilen resmi eşleştiriyoruz
+                    StorageReference reference= firebaseStorage.getReference(path);
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            //veritabanına koyma işlemleri
+                            String downdloadurl= uri.toString();
+                            String comment=binding.commentText.getText().toString();
+                            FirebaseUser user=auth.getCurrentUser();
+                            String email=user.getEmail();
+
+                            HashMap<String,Object> postData= new HashMap<>();
+                            postData.put("userEmail",email);
+                            postData.put("downloadUrl",downdloadurl);
+                            postData.put("comment",comment);
+                            postData.put("date", FieldValue.serverTimestamp());
+
+                            //firebase koleksiyonuna yükleme işlemi ve sonucunun ne olduğunu değerlendirme
+                            firebaseFirestore.collection("Post").add(postData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    goToFeedActivity();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(PostActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(PostActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+        else{
+            Toast.makeText(PostActivity.this,"Görsel seçin",Toast.LENGTH_SHORT).show();
+        }
 
     }
-    private void selectImage(View view){
 
+    public void goToFeedActivity() {
+        Intent intent= new Intent(PostActivity.this,FeedActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    public void selectImage(View view){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){//izin yoksa
+            //iznin neden istenildiğini gösterildiği kısım
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Galeriye gitmek için izin gerekiyor",Snackbar.LENGTH_INDEFINITE).setAction("İzin ver", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                          //izin iste
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }
+            else{
+                //izin iste
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+        else{
+            //zaten izin verilmiş, galeriye git
+            Intent intent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intent);
+        }
+
+    }
+    private void registerLauncher() {
+        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode()==RESULT_OK){
+                    Intent intentFromResult=result.getData();
+                    if (intentFromResult!=null){
+                        imageData=intentFromResult.getData();
+                        binding.imageView.setImageURI(imageData);
+
+                        /*
+                        //bitmap e çevirip yapılmak istenirse;
+                        try {
+                            if (Build.VERSION.SDK_INT>=20){
+                                ImageDecoder.Source source=ImageDecoder.createSource(PostActivity.this.getContentResolver(),imageData);
+                                selectedImage=ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+                            else{
+                                selectedImage=MediaStore.Images.Media.getBitmap(PostActivity.this.getContentResolver(),imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }*/
+                    }
+                }
+            }
+        });
+
+        permissionLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result){
+                    Intent intent= new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intent);
+                }
+                else{
+                    Toast.makeText(PostActivity.this,"İzin verilmedi",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
